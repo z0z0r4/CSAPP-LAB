b1978dd011e4ca4498ee1cacfef21ed9150e7d8f
z0z0r4 <z0z0r4@outlook.com>
2026-04-29 20:08:23 +0800

    2
    Team Name:LoL
    Member 1 :z0z0r4:z0z0r4@outlook.com
    Using default tracefiles in traces/
    Measuring performance with gettimeofday().
    
    Results for mm malloc:
    trace  valid  util     ops      secs  Kops
     0       yes   89%    5694  0.000432 13178
     1       yes   92%    5848  0.000403 14522
     2       yes   95%    6648  0.000511 13002
     3       yes   96%    5380  0.000390 13791
     4       yes   99%   14400  0.000556 25895
     5       yes   88%    4800  0.000663  7240
     6       yes   85%    4800  0.000752  6386
     7       yes   54%   12000  0.031173   385
     8       yes   47%   24000  0.112248   214
     9       yes   26%   14401  0.060728   237
    10       yes   45%   14401  0.003047  4726
    Total          74%  112372  0.210903   533
    
    Perf index = 45 (util) + 36 (thru) = 80/100

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -45,8 +45,14 @@ team_t team = {
 #define CHUNK_HEADER_SIZE 8
 #define CHUNK_FOOTER_SIZE 8
 
+#define CHUNK_NEXT_FREE_P_SIZE 8
+#define CHUNK_PREV_FREE_P_SIZE 8
+
 #define GET(p) (*(size_t *)(p))
 #define PUT(p, val) (*(size_t *)(p) = (val))
+
+#define GET_PTR(p) (*(void **)(p))
+#define PUT_PTR(p, val) (*(void **)(p) = (void *)(val))
 
 #define PACK(size, alloc) ((size) | (alloc))
 
@@ -58,12 +64,36 @@ team_t team = {
 
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
-/*
- * mm_init - initialize the malloc package.
- */
-int mm_init(void)
+// chunk: [size|allocated][next_free_chunk_p][prev_free_chunk_p]
+#define GET_NEXT_FREE_CHUNK(p) ((void *)GET_PTR((char *)p + CHUNK_HEADER_SIZE))
+
+#define GET_PREV_FREE_CHUNK(p) ((void *)GET_PTR((char *)p + CHUNK_HEADER_SIZE + CHUNK_NEXT_FREE_P_SIZE))
+
+#define SMALLEST_CHUNK_SIZE (CHUNK_HEADER_SIZE + CHUNK_FOOTER_SIZE + CHUNK_NEXT_FREE_P_SIZE + CHUNK_PREV_FREE_P_SIZE)
+
+static void *sentinel_chunk_ptr = NULL;
+
+void set_chunk_meta(void *ptr, int chunk_size, int allocated_flag)
 {
-    return 0;
+    PUT(ptr, PACK(chunk_size, allocated_flag));
+    PUT(ptr + chunk_size - CHUNK_FOOTER_SIZE, PACK(chunk_size, allocated_flag));
+}
+
+void clear_chunk_info(void *ptr)
+{
+    set_chunk_meta(ptr, 0, 0);
+}
+
+void set_chunk_next_p(void *ptr, void *next_ptr)
+{
+    // PUT(ptr + CHUNK_HEADER_SIZE, next_ptr);
+    PUT_PTR((char *)ptr + CHUNK_HEADER_SIZE, next_ptr);
+}
+
+void set_chunk_prev_p(void *ptr, void *prev_ptr)
+{
+    // PUT(ptr + CHUNK_HEADER_SIZE + CHUNK_NEXT_FREE_P_SIZE, prev_ptr);
+    PUT_PTR((char *)ptr + CHUNK_HEADER_SIZE + CHUNK_NEXT_FREE_P_SIZE, prev_ptr);
 }
 
 void *get_new_chunk_from_heap(int newsize)
@@ -79,48 +109,159 @@ void *get_new_chunk_from_heap(int newsize)
 
 void *find_free_chunk(int chunk_size)
 {
-    if (mem_heapsize() == 0)
+    if (sentinel_chunk_ptr == NULL)
     {
         return NULL;
     }
 
-    void *ptr = mem_heap_lo();
-    while (ptr < mem_heap_hi() && ptr != NULL)
+    void *ptr = GET_NEXT_FREE_CHUNK(sentinel_chunk_ptr);
+    while (ptr != sentinel_chunk_ptr)
     {
         int current_chunk_size = GET_SIZE(ptr);
         if (current_chunk_size >= chunk_size && !GET_ALLOC(ptr))
         {
             return ptr;
         }
-        ptr = (char *)ptr + current_chunk_size;
-        if (ptr >= mem_heap_hi())
-        {
-            break;
-        }
+        ptr = GET_NEXT_FREE_CHUNK(ptr);
     }
     return NULL;
 }
 
-void set_chunk_meta(void *ptr, int chunk_size, int allocated_flag)
+void insert_chunk_to_free_linked_list(void *ptr)
 {
-    PUT(ptr, PACK(chunk_size, allocated_flag));
-    PUT(ptr + chunk_size - CHUNK_FOOTER_SIZE, PACK(chunk_size, allocated_flag));
+    void *sentinel_chunk_next_chunk_ptr = GET_NEXT_FREE_CHUNK(sentinel_chunk_ptr);
+    // set self
+    set_chunk_next_p(ptr, sentinel_chunk_next_chunk_ptr);
+    set_chunk_prev_p(ptr, sentinel_chunk_ptr);
+
+    // set sentinel_chunk
+    set_chunk_next_p(sentinel_chunk_ptr, ptr);
+
+    // set sentinel_chunk next
+    set_chunk_prev_p(sentinel_chunk_next_chunk_ptr, ptr);
 }
 
-void clear_chunk_info(void *ptr)
+void remove_chunk_from_free_linked_list(void *ptr)
 {
-    set_chunk_meta(ptr, 0, 0);
+    void *prev_chunk_ptr = GET_PREV_FREE_CHUNK(ptr);
+    void *next_chunk_ptr = GET_NEXT_FREE_CHUNK(ptr);
+
+    // set prev chunk next p
+    set_chunk_next_p(prev_chunk_ptr, next_chunk_ptr);
+
+    // set next chunk prev p
+    set_chunk_prev_p(next_chunk_ptr, prev_chunk_ptr);
 }
 
-void separate_free_chunk(void *ptr, int chunk_size, int new_chunk_size)
+void separate_free_chunk(void *ptr, int new_chunk_size)
 {
-    if (chunk_size >= new_chunk_size + CHUNK_HEADER_SIZE + CHUNK_FOOTER_SIZE + SIZE_T_SIZE)
+    int current_chunk_size = GET_SIZE(ptr);
+    if (current_chunk_size >= new_chunk_size + SMALLEST_CHUNK_SIZE)
     {
         set_chunk_meta(ptr, new_chunk_size, 0);
         void *new_chunk_ptr = (char *)ptr + new_chunk_size;
-        int remaining_chunk_size = chunk_size - new_chunk_size;
+        int remaining_chunk_size = current_chunk_size - new_chunk_size;
         set_chunk_meta(new_chunk_ptr, remaining_chunk_size, 0);
+        insert_chunk_to_free_linked_list(new_chunk_ptr);
     }
+}
+
+int mm_checkheap(int verbose) {
+    return 1;
+    void *bp;
+    int implicit_free_count = 0;
+    int explicit_free_count = 0;
+
+    // ---------------------------------------------------------
+    // 1. 隐式遍历：从 Sentinel Chunk 开始，按内存地址顺序遍历整个堆
+    // ---------------------------------------------------------
+    bp = sentinel_chunk_ptr;
+    
+    // 假设 sentinel_chunk 是堆的最开头，遍历直到堆顶
+    while (bp != NULL && bp < mem_heap_hi()) {
+        int size = GET_SIZE(bp);
+        int alloc = GET_ALLOC(bp);
+
+        // 检查1：块大小不能为0，否则会导致死循环
+        if (size == 0) {
+            printf("Heap Error: Chunk size is 0 at %p\n", bp);
+            return 0;
+        }
+
+        // 检查2：Header 和 Footer 是否匹配
+        if (GET(bp) != GET((char *)bp + size - CHUNK_FOOTER_SIZE)) {
+            printf("Heap Error: Header and Footer mismatch at %p\n", bp);
+            return 0;
+        }
+
+        // 统计真实的空闲块数量（跳过哨兵块）
+        if (!alloc && bp != sentinel_chunk_ptr) {
+            implicit_free_count++;
+        }
+
+        // 步进到下一个块
+        bp = (char *)bp + size; 
+    }
+
+    // ---------------------------------------------------------
+    // 2. 显式遍历：沿着双向链表的 next 指针进行遍历
+    // ---------------------------------------------------------
+    if (sentinel_chunk_ptr != NULL) {
+        void *free_bp = GET_NEXT_FREE_CHUNK(sentinel_chunk_ptr);
+        
+        while (free_bp != sentinel_chunk_ptr && free_bp != NULL) {
+            // 检查3：在空闲链表中的块，其 allocated 标志位必须是 0
+            if (GET_ALLOC(free_bp)) {
+                printf("Heap Error: Allocated block found in free list at %p\n", free_bp);
+                return 0;
+            }
+            
+            // 检查4：双向链表指针的一致性 (A->next->prev 必须等于 A)
+            void *next = GET_NEXT_FREE_CHUNK(free_bp);
+            void *prev = GET_PREV_FREE_CHUNK(free_bp);
+            
+            if (GET_PREV_FREE_CHUNK(next) != free_bp) {
+                printf("Heap Error: next->prev != current at %p\n", free_bp);
+                return 0;
+            }
+            if (GET_NEXT_FREE_CHUNK(prev) != free_bp) {
+                printf("Heap Error: prev->next != current at %p\n", free_bp);
+                return 0;
+            }
+
+            explicit_free_count++;
+            free_bp = next;
+        }
+    }
+
+    // ---------------------------------------------------------
+    // 3. 交叉比对：检查是否有空闲块“迷路”了
+    // ---------------------------------------------------------
+    if (implicit_free_count != explicit_free_count) {
+        printf("Heap Error: Free block count mismatch! Implicit: %d, Explicit: %d\n", 
+               implicit_free_count, explicit_free_count);
+        return 0;
+    }
+
+    if (verbose) {
+        printf("Heap is consistent. Total free blocks: %d\n", explicit_free_count);
+    }
+    
+    return 1;
+}
+
+/*
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void)
+{
+    // sentinel chunk
+    int sentinel_chunk_size = SMALLEST_CHUNK_SIZE;
+    sentinel_chunk_ptr = get_new_chunk_from_heap(sentinel_chunk_size);
+    set_chunk_meta(sentinel_chunk_ptr, sentinel_chunk_size, 1);
+    set_chunk_next_p(sentinel_chunk_ptr, sentinel_chunk_ptr);
+    set_chunk_prev_p(sentinel_chunk_ptr, sentinel_chunk_ptr);
+    return 0;
 }
 
 /*
@@ -129,7 +270,7 @@ void separate_free_chunk(void *ptr, int chunk_size, int new_chunk_size)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + CHUNK_HEADER_SIZE + CHUNK_FOOTER_SIZE);
+    int newsize = (size >= (CHUNK_NEXT_FREE_P_SIZE + CHUNK_PREV_FREE_P_SIZE)) ? ALIGN(size + CHUNK_HEADER_SIZE + CHUNK_FOOTER_SIZE) : SMALLEST_CHUNK_SIZE;
 
     void *ptr = find_free_chunk(newsize);
     if (ptr == NULL)
@@ -148,9 +289,10 @@ void *mm_malloc(size_t size)
     {
         int current_chunk_size = GET_SIZE(ptr);
         // if the remaining free chunk is big enough to hold the meta info and at least 1 bytes, then separate the chunk
-        if (current_chunk_size >= newsize + CHUNK_HEADER_SIZE + CHUNK_FOOTER_SIZE + SIZE_T_SIZE)
+        remove_chunk_from_free_linked_list(ptr);
+        if (current_chunk_size >= newsize + SMALLEST_CHUNK_SIZE)
         {
-            separate_free_chunk(ptr, current_chunk_size, newsize);
+            separate_free_chunk(ptr, newsize);
             set_chunk_meta(ptr, newsize, 1);
         }
         else
@@ -181,15 +323,17 @@ void mm_free(void *ptr)
     int prev_chunk_size = 0;
     int next_chunk_size = 0;
 
-    if (prev_chunk_ptr >= mem_heap_lo() && !GET_ALLOC(prev_chunk_ptr))
+    void *new_free_chunk_ptr = NULL;
+
+    if (prev_chunk_ptr >= mem_heap_lo())
     {
-        prev_chunk_free_flag = GET_ALLOC(prev_chunk_ptr);
+        prev_chunk_free_flag = !GET_ALLOC(prev_chunk_ptr);
         prev_chunk_size = GET_SIZE(prev_chunk_ptr);
     }
 
-    if (next_chunk_ptr < mem_heap_hi() && !GET_ALLOC(next_chunk_ptr))
+    if (next_chunk_ptr < mem_heap_hi())
     {
-        next_chunk_free_flag = GET_ALLOC(next_chunk_ptr);
+        next_chunk_free_flag = !GET_ALLOC(next_chunk_ptr);
         next_chunk_size = GET_SIZE(next_chunk_ptr);
     }
 
@@ -197,6 +341,7 @@ void mm_free(void *ptr)
     {
         // just set current chunk to free
         set_chunk_meta(chunk_ptr, chunk_size, 0);
+        new_free_chunk_ptr = chunk_ptr;
     }
     else if (prev_chunk_free_flag && !next_chunk_free_flag)
     {
@@ -204,13 +349,17 @@ void mm_free(void *ptr)
         int merged_chunk_size = chunk_size + prev_chunk_size;
         clear_chunk_info(chunk_ptr);
         set_chunk_meta(prev_chunk_ptr, merged_chunk_size, 0);
+        remove_chunk_from_free_linked_list(prev_chunk_ptr);
+        new_free_chunk_ptr = prev_chunk_ptr;
     }
     else if (!prev_chunk_free_flag && next_chunk_free_flag)
     {
         // merge with next
         int merged_chunk_size = chunk_size + next_chunk_size;
-        clear_chunk_info(chunk_ptr);
-        set_chunk_meta(next_chunk_ptr, merged_chunk_size, 0);
+        clear_chunk_info(next_chunk_ptr);
+        set_chunk_meta(chunk_ptr, merged_chunk_size, 0);
+        remove_chunk_from_free_linked_list(next_chunk_ptr);
+        new_free_chunk_ptr = chunk_ptr;
     }
     else
     {
@@ -219,6 +368,14 @@ void mm_free(void *ptr)
         clear_chunk_info(chunk_ptr);
         clear_chunk_info(next_chunk_ptr);
         set_chunk_meta(prev_chunk_ptr, merged_chunk_size, 0);
+        remove_chunk_from_free_linked_list(prev_chunk_ptr);
+        remove_chunk_from_free_linked_list(next_chunk_ptr);
+        new_free_chunk_ptr = prev_chunk_ptr;
+    }
+
+    if (new_free_chunk_ptr != NULL)
+    {
+        insert_chunk_to_free_linked_list(new_free_chunk_ptr);
     }
 }
 
